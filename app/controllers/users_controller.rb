0f741249b9ead9b9d8a299ef302b6fa5a3477d89
Nick Inman <nick@theoatmeal.com>
2019-02-19 23:51:58 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,11 @@
 class UsersController < ApplicationController
 
   def create
-    binding.pry
     user = User.new(user_params)
     if user.name.nil? | user.name == ''
-
       redirect_to sessions_new_path
     else
       user.save
-
       session[:user_id] = user.id
     end
   end
