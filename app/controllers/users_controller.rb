4b3dfed227b78fb12ea98a7fcfb4ac69d7b6a975
Nick Inman <nick@theoatmeal.com>
2019-02-19 23:55:47 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    binding.pry
     if user.valid?
       user.save
       session[:user_id] = user.id
