57812551bf6928f5a06c479779c99f8ede046667
Nick Inman <nick@theoatmeal.com>
2019-02-19 23:51:05 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:user][:name])
-    @user.authenticate(params[:user][:password])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
+
   end
 end
