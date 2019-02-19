66556cfbc5895b090aa490f0537a01d5a174e758
Nick Inman <nick@theoatmeal.com>
2019-02-19 23:44:19 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-
+    if user.name.nil? | user.name == ''
+      redirect_to users_new_path
     binding.pry
   end
 
